92135cf39f0f27275bba5d96cc4322ebae180197
misterabvir <abvir@yandex.ru>
2023-01-18 09:42:12 +0300

    updated sln for task 13 to a simpler one

--- Seminar_02/Homework_task_13/Program.cs
+++ Seminar_02/Homework_task_13/Program.cs
@@ -7,25 +7,25 @@
 **************************************************************/
 
 //solution with string manipulation... cheating... but it's work
-string GetThirdStringDigit(string data) => data.Length >= 3 ? data[2].ToString() : "no third digit";
+string GetThirdDigitFromString(string data) => data.Length >= 3 ? data[2].ToString() : "no third digit";
 
 //solution with numbers
-string GetThirdIntDigit(int data)
+string GetThirdDigitFromInteger(int data)
 {
-    int lim = 100;
-    if(data < lim) return "no third digit";    
-    while(lim < data)               // for 3 digit number -> 1000, for 4 -> 10000, for 5 -> 100000 .... 
-        lim *= 10;    
-    lim /= 1000;                    // for 3 digit number -> 1,    for 4 -> 10,    for 5 -> 100 ....   
-    return ((data / lim) % 10).ToString();
+    if(data < 100) return "no third digit";    
+    while(data > 1000)                
+        data /= 10;                   
+    return (data % 10).ToString();
 }
 
+/*******************************************************************************************************/
 
 Console.Write("Input your number: ");
-string? sNumber = Console.ReadLine();
-int iNumber = Convert.ToInt32(sNumber);
 
 //check first cheating way 
-Console.WriteLine($"{sNumber ?? ""} -> {GetThirdStringDigit(sNumber ?? "")}"); //operator ?? to avoid throwing warnings
+string? sNumber = Console.ReadLine();
+Console.WriteLine($"1: {sNumber ?? ""} -> {GetThirdDigitFromString(sNumber ?? "")}"); //operator ?? to avoid throwing warnings
+
 //check second way 
-Console.WriteLine($"{iNumber} -> {GetThirdIntDigit(iNumber)}");
+int iNumber = Convert.ToInt32(sNumber);
+Console.WriteLine($"2: {iNumber} -> {GetThirdDigitFromInteger(iNumber)}");
