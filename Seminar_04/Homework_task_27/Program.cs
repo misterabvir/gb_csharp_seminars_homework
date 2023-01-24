bb16b00f7fcaf9891e6d6e7d67eed8309acb9782
misterabvir <abvir@yandex.ru>
2023-01-24 22:57:53 +0300

    added sln homework task 27

---
+++ Seminar_04/Homework_task_27/Program.cs
@@ -0,0 +1,28 @@
+﻿/***************************************************
+* Задача 27: Напишите программу, которая принимает *
+* на вход число и выдаёт сумму цифр в числе.       *
+****************************************************/
+int SumOfDigits(int data)
+{
+    int sum = 0;
+    while(data > 0)
+    {
+        sum += data % 10;
+        data /= 10;
+    }
+    return sum;
+}
+
+int GetInput()
+{
+    int input = 0;   
+    do{
+        Console.WriteLine($"Enter some number (between {int.MinValue} and {int.MaxValue}): ");
+    }while(!int.TryParse(Console.ReadLine(), out input));
+    return input;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+int number = GetInput();
+Console.WriteLine($"The sum of the digits in number {number} is {SumOfDigits(number)}"); 
+
